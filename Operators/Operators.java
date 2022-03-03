950b0d8ab57b04bffa750a0a676cb9653644d39d
AyanF <ayanfarooqui211@gmail.com>
2022-03-03 20:15:14 +0530

    Ternary Operators

---
+++ Operators/Operators.java
@@ -0,0 +1,17 @@
+public class Operators {
+
+   public static void main(String args[]) {
+
+      int firstNum, secondNum;
+
+      firstNum = 10;
+
+      secondNum = (firstNum == 1) ? 20: 30;
+
+      System.out.println( secondNum);
+
+      secondNum = (firstNum == 10) ? 20: 30;
+
+      System.out.println(  secondNum);
+   }
+}
